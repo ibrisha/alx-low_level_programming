62a80070fb1fc338ae59229544d82789e0774411
Ibrahim <ibrahimbsc8@gmail.com>
2021-08-31 11:39:30 -0700

    touche all files and done 0-holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *holberton - prints the string "Holberton" from char array
+ *Return:0 on success
+ */
+int main(void)
+{
+	char ch[] = "Holberton";
+	int i;
+	for(i = 0;i < 9;i++)
+	{
+		_putchar(ch[i]);
+
+	}
+	_putchar('\n');
+	return (0);
+
+}

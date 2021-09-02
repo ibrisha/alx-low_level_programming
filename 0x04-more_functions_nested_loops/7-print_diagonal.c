4afefea91b32de904a0372ad9c8fe3de99981148
Ibrahim <ibrahimbsc8@gmail.com>
2021-09-02 11:51:29 -0700

    diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,13 +6,13 @@
  */
 void print_diagonal(int n)
 {
-	int i,j;
+	int i, j;
 
 	if (n > 0)
 	{
-		for (i = 0;i < n;i++)
+		for (i = 0; i < n; i++)
 		{
-			for (j = 0;j <= i;j++)
+			for (j = 0; j <= i; j++)
 			{
 				if (j == i)
 					_putchar('\\');

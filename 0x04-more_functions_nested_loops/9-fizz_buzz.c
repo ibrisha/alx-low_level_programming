960bbd258f194c0724f820561d6c0b6009470960
Ibrahim <ibrahimbsc8@gmail.com>
2021-09-02 13:32:15 -0700

    fizz-buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,8 @@
 #include < stdio.h >
 /**
- * fizz_buzz - prints numbers from 1-100.
+ *fizz_buzz - prints numbers from 1-100.
  *if number is multiple of 3 it prints Fizz.
- **if number is multiple of 5 it prints Buzz.
+ *if number is multiple of 5 it prints Buzz.
  * Return: Nothing.
  */
 void fizz_buzz(void)
@@ -11,12 +11,12 @@ void fizz_buzz(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
-		else
+		if (i % 3 != 0 && i % 5 != 0)
 			printf("%d ", i);
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("Buzz ");
 	}
 	printf("\n");
 }

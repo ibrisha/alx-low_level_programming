de6655c98acd702ca47fb1e066906fc8bf7087ee
Ibrahim <ibrahimbsc8@gmail.com>
2021-09-06 02:06:19 -0700

    reset value using pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,12 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ *reset_to_98 - resets the value of integer using pointer
+ *@n:integer value to resetted
+ */
+void reset_to_98(int *n)
+{
+	int *p = &n;
+
+	*p = 98;
+}

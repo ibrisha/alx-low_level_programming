bfa1af9fe46c7a2c26f81f7cdf30832571097798
Ibrahim <ibrahimbsc8@gmail.com>
2021-09-06 12:34:04 -0700

    string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,14 @@
 #include "main.h"
+/**
+ *_strlen - counts number of characters of string
+ *@s:pointer to string to be counted
+ *Return:returns integer count of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s(len) != '\0')
+		len + = 1;
+	return (len);
+}

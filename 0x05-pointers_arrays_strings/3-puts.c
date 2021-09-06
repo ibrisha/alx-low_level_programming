551b2cbd94f042e0551e88e9524d6b35feead94f
Ibrahim <ibrahimbsc8@gmail.com>
2021-09-06 13:03:28 -0700

    print string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,17 @@
 #include "main.h"
+/**
+ *_puts - prints string to standard output followed by newline
+ *@str:pointer to string
+ *Return:Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}

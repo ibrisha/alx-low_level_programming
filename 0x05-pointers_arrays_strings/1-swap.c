180d46a21c7f09d323538d0130c9074d364e60da
Ibrahim <ibrahimbsc8@gmail.com>
2021-09-06 12:04:25 -0700

    sawp using pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,15 @@
 #include "main.h"
+/**
+ *swap_int - swaps two integers
+ *@a:integer value
+ *@b:integer value
+ *Return:Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}

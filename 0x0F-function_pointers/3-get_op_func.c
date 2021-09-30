50fe7c05774d1f67383618db2ee7d95b20f989aa
Ibrahim <ibrahimbsc8@gmail.com>
2021-09-30 07:08:19 -0700

    fix bug

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -6,8 +6,6 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	int i = 0;
-
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -16,12 +14,15 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+	int i;
 
+	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (s == ops[i].op)
-			return(ops[i].f);
+		if (*s == *(ops[i].op) && s[1] == '\0')
+			return (ops[i].f);
 		i++;
 	}
-
+	printf("Error\n");
+	exit(99);
 }
